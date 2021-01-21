bac952b9989caaf1e9dc74f840502c75939a712a
alekseik1 <1alekseik1@gmail.com>
2021-01-21 23:30:18 +0300

    feat(problem3): problem + tests

---
+++ tests/test_problem_3.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pytest
+from pathlib import Path
+from problem3.solutions import task1, task2, task3
+
+
+basedir = Path(__file__).parent.parent/'tests'/'arrays'
+
+
+@pytest.mark.parametrize('array, func', [
+    (np.load(basedir / 'p3t1.npy'), task1),
+    (np.load(basedir / 'p3t2.npy'), task2),
+    (np.load(basedir / 'p3t3.npy'), task3),
+])
+def test_problem_3(array, func):
+    np.testing.assert_allclose(func(), array)
